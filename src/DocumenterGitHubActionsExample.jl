c11eb00acef83c59a5c6738b020cf18f4fccd9b9
Morten Piibeleht <morten.piibeleht@gmail.com>
2020-08-25 10:54:30 +1200

    Add a function

--- src/DocumenterGitHubActionsExample.jl
+++ src/DocumenterGitHubActionsExample.jl
@@ -1,5 +1,12 @@
 module DocumenterGitHubActionsExample
 
 # Write your package code here.
+function foo(x)
+    y = x^2
+    if x % 2 == 0
+        y = -1
+    end
+    return y+1
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
-using DocumenterGitHubActionsExample
+using DocumenterGitHubActionsExample: foo
 using Test
 
 @testset "DocumenterGitHubActionsExample.jl" begin
     # Write your tests here.
+    @test foo(1) == 2
+    @test foo(3) == 10
 end
